141b51ee1ce0d8197ddb445654ed040bd99f56f7
Endy Muhardin <endy.muhardin@gmail.com>
2013-06-17 11:34:23 +0700

    implementasi proses bisnis (baru template saja)

---
+++ penagihan-service/src/main/java/com/artivisi/penagihan/service/PenagihanServiceImpl.java
@@ -0,0 +1,35 @@
+package com.artivisi.penagihan.service;
+
+import java.util.List;
+
+import com.artivisi.penagihan.domain.Nasabah;
+import com.artivisi.penagihan.domain.PenagihanService;
+
+public class PenagihanServiceImpl implements PenagihanService {
+
+	public void simpan(Nasabah n) {
+		// TODO Auto-generated method stub
+		
+	}
+
+	public Nasabah cariNasabahById(String id) {
+		// TODO Auto-generated method stub
+		return null;
+	}
+
+	public Nasabah cariNasabahByNomer(String nomer) {
+		// TODO Auto-generated method stub
+		return null;
+	}
+
+	public Long hitungSemuaNasabah() {
+		// TODO Auto-generated method stub
+		return null;
+	}
+
+	public List<Nasabah> cariSemuaNasabah(Integer start, Integer rows) {
+		// TODO Auto-generated method stub
+		return null;
+	}
+
+}
